1deb84d57edb2701ef35d1b0e8c16e260ef19c6c
wh0smarg0 <wh0smarg0.s@gmail.com>
2025-12-12 10:46:44 +0200

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,3 +1,4 @@
+<?php $start_time = microtime(true); // ДОДАНО: Початок вимірювання часу (Пункт 4) ?>
 <?php include "common.php"; ?>
 <!DOCTYPE html>
 <html lang="uk">
@@ -6,12 +7,12 @@
     <title>Сторінка 2</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body data-page="page2.php">
+
 <div class="container">
 
-    <!-- Блок 1 (шапка + X + меню) -->
     <div class="header">
-        <div class="x"><?php echo $x; ?></div>
+        <div class="x data-editable" id="content_x"><?php echo $x; ?></div>
         <div class="menu">
             <ul>
                 <?php foreach($menu as $link => $title): ?>
@@ -21,24 +22,32 @@
         </div>
     </div>
 
-    <!-- Блок 2 (ліва колонка) -->
-    <div class="left"><?php echo $texts[0]; ?></div>
+    <div class="left data-editable" id="content_t0"><?php echo $texts[0]; ?></div>
 
-    <!-- Блок 3 (центральний) -->
-    <div class="main"><?php echo $texts[1]; ?></div>
+    <div class="main data-editable" id="content_t1"><?php echo $texts[1]; ?></div>
 
-    <!-- Блок 4 -->
-    <div class="right-top"><?php echo $texts[2]; ?></div>
+    <div class="right-top data-editable" id="content_t2"><?php echo $texts[2]; ?></div>
 
-    <!-- Блок 5 -->
-    <div class="right-bottom"><?php echo $texts[3]; ?></div>
+    <div class="right-bottom data-editable" id="content_t3"><?php echo $texts[3]; ?></div>
 
-    <!-- Блок 6 (футер + Y) -->
     <div class="footer">
-        <div class="y"><?php echo $y; ?></div>
-        <div><?php echo $texts[4]; ?></div>
+        <div class="y data-editable" id="content_y"><?php echo $y; ?></div>
+        <div class="footer-text data-editable" id="content_t4"><?php echo $texts[4]; ?></div>
     </div>
 
 </div>
+
+<script src="script.js"></script>
+
+<?php
+// Обчислення часу (Пункт 4)
+$end_time = microtime(true);
+$execution_time = $end_time - $start_time;
+?>
+
+<div class="time-info">
+    Час формування сторінки: <?php echo round($execution_time, 4); ?> сек.
+</div>
+
 </body>
 </html>
